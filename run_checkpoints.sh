7dd6b54fbb9f67c4527eb9229d11a5c4916d196f
Hamel Husain <hamel.husain@gmail.com>
2020-07-10 14:25:55 -0700

    save workflow

--- run_checkpoints.sh
+++ run_checkpoints.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-IFS=','
-checkpoints='passing_checkpoint,failing_checkpoint'
 
-for c in $checkpoints;do
+# Validate That Required Inputs Were Supplied
+function check_env() {
+    if [ -z $(eval echo "\$$1") ]; then
+        echo "Variable $1 not found.  Exiting..."
+        exit 1
+    fi
+}
+
+check_env "INPUT_CHECKPOINTS"
+
+# Loop through checkpoints
+IFS=','
+for c in $INPUT_CHECKPOINTS;do
     great_expectations checkpoint run $c
 done
