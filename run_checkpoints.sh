8f616c1733a954701097c98e1084ecbced92736a
Hamel Husain <hamel.husain@gmail.com>
2020-07-21 00:42:35 -0700

    scaffolding for commenting

--- run_checkpoints.sh
+++ run_checkpoints.sh
@@ -40,25 +40,46 @@ for c in $INPUT_CHECKPOINTS;do
     fi
 done
 
-# Build the ephemeral docs site
+# Build the ephemeral docs site and read location of site into DOCS_LOC
 python /build_gh_action_site.py
 DOCS_LOC=`cat _temp_greatexpectations_action_docs_location_dir.txt`
+
+# Emit Failing and Passing Checkpoints as output variables
 echo "::set-output name=FAILING_CHECKPOINTS::${FAILING_CHECKPOINTS}"
 echo "::set-output name=PASSING_CHECKPOINTS::${PASSING_CHECKPOINTS}"
 
 # Optionally launch docs on Netlify
-if [[ ! -z "$NETLIFY_AUTH_TOKEN" ]] && [[ ! -z "$NETLIFY_SITE_ID" ]]; 
-then
-    [[  -z "$DOCS_LOC" ]] && { echo "Variable DOCS_LOC is empty" ; exit 1; }
-    [[ ! -d "$DOCS_LOC" ]] && { echo "Directory specified in variable DOCS_LOC: ${DOCS_LOC} does not exist."; exit 1; }
-    netlify deploy --dir $DOCS_LOC | tee _netlify_logs.txt
-    # Parse URL from logs and send to next step
-    DOCS_URL=`cat _netlify_logs.txt | awk '/Draft URL: /{print $4}'`
-    ## find out if it is empty or not
-    [[  -z "$DOCS_URL" ]] && { echo "Variable DOCS_URL is empty" ; exit 1; }
-    echo "::set-output name=NETLIFY_DOCS_URL::${DOCS_URL}"
-else
-    echo "Netlify Deploy Skipped."
+if [[ ! -z "$INPUT_NETLIFY_AUTH_TOKEN" ]] && [[ ! -z "$INPUT_NETLIFY_SITE_ID" ]]; 
+    then
+        # Set Inputs to proper environment variable names for Netlify CLI
+        export NETLIFY_AUTH_TOKEN=$INPUT_NETLIFY_AUTH_TOKEN
+        export NETLIFY_SITE_ID=$INPUT_NETLIFY_SITE_ID
+
+        # Verify Docs Directory
+        [[  -z "$DOCS_LOC" ]] && { echo "Variable DOCS_LOC is empty" ; exit 1; }
+        [[ ! -d "$DOCS_LOC" ]] && { echo "Directory specified in variable DOCS_LOC: ${DOCS_LOC} does not exist."; exit 1; }
+
+        # Deploy docs site to Netlify, and write out to logs
+        netlify deploy --dir $DOCS_LOC | tee _netlify_logs.txt
+
+        # Parse URL from logs and send to next step
+        DOCS_URL=`cat _netlify_logs.txt | awk '/Draft URL: /{print $4}'`
+
+        ## Verify URL exists and emit it as an output variable
+        [[  -z "$DOCS_URL" ]] && { echo "Variable DOCS_URL is empty" ; exit 1; }
+        echo "::set-output name=NETLIFY_DOCS_URL::${DOCS_URL}"
+    else
+        echo "Netlify Deploy Skipped."
+fi
+
+if [[ ! -z "$INPUT_PR_COMMENT_ON_ERROR" ]] && [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
+    if [[ ! -z "$GITHUB_HEAD_REF" ]]; then
+            echo "::warning::pull request comments are not supported on a push made from a forked repository for security reasons."
+        elif [[ -z "$INPUT_GITHUB_TOKEN" ]]; then
+            echo "::warning::You must supply the input GITHUB_TOKEN to trigger a pull request comment."
+        else
+            echo "TODO: comment on the issue!"
+    fi
 fi
 
 # exit with appropriate status
--- test-action.sh
+++ test-action.sh
@@ -6,6 +6,9 @@ docker build -t test-ge-action .
 INPUT_CHECKPOINTS="passing_checkpoint,failing_checkpoint,passing_checkpoint"
 
 docker run -e INPUT_CHECKPOINTS=$INPUT_CHECKPOINTS \
--e NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN \
--e NETLIFY_SITE_ID=$NETLIFY_SITE_ID \
+-e INPUT_NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN \
+-e INPUT_NETLIFY_SITE_ID=$NETLIFY_SITE_ID \
+-e INPUT_PR_COMMENT_ON_ERROR="true" \
+-e GITHUB_EVENT_NAME="push" \
+-e INPUT_GITHUB_TOKEN="foo" \
 -v $PWD:/app test-ge-action
