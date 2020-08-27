b9a16a291fbeed4ee0b6297e990f0a58aa7cfb85
Hamel Husain <hamel.husain@gmail.com>
2020-08-27 14:52:30 -0700

    correct conditional

--- run_checkpoints.sh
+++ run_checkpoints.sh
@@ -15,7 +15,7 @@ if [[ ! -z "$INPUT_GE_HOME" ]]; then
     cd ${INPUT_GE_HOME}
 fi
 
-great_expectations suite list || echo "::error::An error occured while loading your great expectations project. See action logs for more detail." && exit 1;
+great_expectations suite list || (echo "::error::An error occured while loading your great expectations project. See action logs for more detail." && exit 1);
 
 # Loop through checkpoints
 STATUS=0
